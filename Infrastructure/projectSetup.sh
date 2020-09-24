46628673b67b8fe740c1e8426a5f4ee168f66a31
Blake Alvernaz <balverna@gmail.com>
2020-09-24 15:59:09 -0700

    Main Project Setup and Completed

--- Infrastructure/projectSetup.sh
+++ Infrastructure/projectSetup.sh
@@ -1,10 +1,32 @@
 #!/usr/bin/env bash
 
+# Script File To Create the Google Cloud Project
+
 source setupVariables.sh
+ 
+ # Check if your Project exist if not Create New Project
 
 if ! gcloud projects list | grep -q $projectId; then
     gcloud projects create $projectId
 fi
 
-gcloud config set project $projectId
+# Check if Your Project is set in the config, If not set it to this Project
 
+if ! gcloud config list | grep -q $projectId; then
+    gcloud config set project $projectId
+fi
+
+# Check if Kubernetes api is Enabled, if not enable it
+
+if ! gcloud services list --enabled | grep -q $kubeApi; then
+    gcloud services enable $kubeApi
+fi
+
+# Checks if you have a Cluster, if not creates one
+
+if ! gcloud container clusters list | grep  -q $clusterName; then 
+gcloud beta container --project "cloudmediafeeder" clusters create $clusterName --zone $myZone --no-enable-basic-auth --cluster-version "1.15.12-gke.20" --machine-type "g1-small" --image-type "COS" --disk-type "pd-standard" --disk-size "100" --metadata disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --num-nodes "1" --enable-stackdriver-kubernetes --enable-ip-alias --network "projects/cloudmediafeeder/global/networks/default" --subnetwork "projects/cloudmediafeeder/regions/us-west1/subnetworks/default" --default-max-pods-per-node "110" --no-enable-master-authorized-networks --addons HorizontalPodAutoscaling,HttpLoadBalancing --enable-autoupgrade --enable-autorepair --max-surge-upgrade 1 --max-unavailable-upgrade 0
+
+fi
+
+gcloud container cluster get-credentials $clusterName --zone=$myZone
--- Infrastructure/setupVariables.sh
+++ Infrastructure/setupVariables.sh
@@ -3,5 +3,5 @@
 myZone="us-west1-a"
 myRegion="us-west1"
 projectId="cloudmediafeeder"
-
-
+kubeApi="container.googleapis.com"
+clusterName="feeders"
