0e6c875d72be7df6d779ee2cc6293931e6a98a8e
Blake Alvernaz <balverna@gmail.com>
2020-09-28 15:41:15 -0700

    Succucess Fully Created a Sonarr Pod

--- Infrastructure/projectSetup.sh
+++ Infrastructure/projectSetup.sh
@@ -25,8 +25,10 @@ fi
 # Checks if you have a Cluster, if not creates one
 
 if ! gcloud container clusters list | grep  -q $clusterName; then 
-gcloud beta container --project "cloudmediafeeder" clusters create $clusterName --zone $myZone --no-enable-basic-auth --cluster-version "1.15.12-gke.20" --machine-type "g1-small" --image-type "COS" --disk-type "pd-standard" --disk-size "100" --metadata disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --num-nodes "1" --enable-stackdriver-kubernetes --enable-ip-alias --network "projects/cloudmediafeeder/global/networks/default" --subnetwork "projects/cloudmediafeeder/regions/us-west1/subnetworks/default" --default-max-pods-per-node "110" --no-enable-master-authorized-networks --addons HorizontalPodAutoscaling,HttpLoadBalancing --enable-autoupgrade --enable-autorepair --max-surge-upgrade 1 --max-unavailable-upgrade 0
+gcloud beta container --project $projectId clusters create $clusterName --zone $myZone --no-enable-basic-auth --cluster-version "1.15.12-gke.20" --machine-type "g1-small" --image-type "COS" --disk-type "pd-standard" --disk-size "500" --metadata disable-legacy-endpoints=true --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --num-nodes "1" --enable-stackdriver-kubernetes --enable-ip-alias --network "projects/cloudmediafeeder/global/networks/default" --subnetwork "projects/cloudmediafeeder/regions/us-west1/subnetworks/default" --default-max-pods-per-node "110" --no-enable-master-authorized-networks --addons HorizontalPodAutoscaling,HttpLoadBalancing --enable-autoupgrade --enable-autorepair --max-surge-upgrade 1 --max-unavailable-upgrade 0
 
 fi
 
-gcloud container cluster get-credentials $clusterName --zone=$myZone
+gcloud container clusters get-credentials $clusterName --zone=$myZone
+
+kubectl apply -f ../sonarr.yaml
